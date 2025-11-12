02367af8349a9b467f7fb43df98b85000ec0d3f2
El-Tomaz <tomazlemosgarcia@gmail.com>
2025-11-12 14:36:31 -0300

    src: consegui sincronizar horario com sntp e rtc erro: Quando api atinge quota maxima de envios, trava envio de dados, isso por algum motivo esta travando de enviar dados para queue o que ocasiona perda de informações; src: tasks separados para aquisição e envio de dados todo: datalog com spiffs

--- teste_csv/teste_csv.ino
+++ teste_csv/teste_csv.ino
@@ -5,6 +5,7 @@
 #include "time.h"
 
 #include <freertos/task.h>
+#include <freertos/queue.h>
 
 #include <ESP_Google_Sheet_Client.h>
 
@@ -18,50 +19,80 @@
 
 #define tpCS1 18
 
-#define tpNUMBER 1 //number of thermocouples used 
+#define tpNUMBER 1  //number of thermocouples used
 
-MAX6675 thermocouples[tpNUMBER] = {MAX6675(tpCLK, tpCS1,tpDO)};
+MAX6675 thermocouples[tpNUMBER] = { MAX6675(tpCLK, tpCS1, tpDO) };
 
-struct tm timeinfo;
+typedef struct {
+  tm time;
+  float temp[tpNUMBER];
+} data;
+
+QueueHandle_t dados = NULL;
+
 void tokenStatusCallback(TokenInfo info);
 
+void read_temp(void *pv) {
+  data item;
+  time_t now = time(NULL);
+  struct tm timeinfo;
+
+  while (true) {
+    time(&now);
+    localtime_r(&now, &timeinfo);
+
+    item.time = timeinfo;
+
+    for (int i = 0; i < tpNUMBER; i++) {
+      item.temp[i] = thermocouples[i].readCelsius();
+    }
+
+    if (xQueueSend(dados, (void *)&item, pdMS_TO_TICKS(100)) != pdPASS) {
+      Serial.println("read_temp> erro: Falha ao colocar item na Queue");
+    }
+
+    vTaskDelay(pdMS_TO_TICKS(500));
+  }
+}
 
 void send_data(void *pv) {
   time_t now;
   bool ready = false;
-  char horario[9];
+
   FirebaseJson valueRange;
   FirebaseJson response;
 
   while (true) {
     ready = GSheet.ready();
+    data item;
+    char horario[9];
     if (ready) {
-      
-      time_t tt = time(NULL);
-      timeinfo = *gmtime(&tt);
 
-      Serial.println("Gsheet ready!");
-      snprintf(horario, 9, "%d:%d:%d", timeinfo.tm_hour, timeinfo.tm_min, timeinfo.tm_sec);
-      Serial.println(horario);
-      valueRange.add("majorDimension", "COLUMNS");
-      valueRange.set("values/[0]/[0]", horario);
+      if (xQueueReceive(dados, (void *)&item, pdMS_TO_TICKS(100)) != pdPASS) {
+        Serial.println("Falha ao receber dados!");
+      } else {
+        tm timeinfo = item.time;
+        Serial.println("Gsheet ready!");
+        snprintf(horario, 9, "%d:%d:%d", timeinfo.tm_hour, timeinfo.tm_min, timeinfo.tm_sec);
+        Serial.println(horario);
+        valueRange.add("majorDimension", "COLUMNS");
+        valueRange.set("values/[0]/[0]", horario);
 
-      for(int i = 0; i < tpNUMBER; i++){
-        char key[16] = "";
-        snprintf(key, 16, "values/[%d]/[0]", i+ 1);
-        valueRange.set(key, String(thermocouples[i].readCelsius()));
-      }
+        for (int i = 0; i < tpNUMBER; i++) {
+          char key[16] = "";
+          snprintf(key, 16, "values/[%d]/[0]", i + 1);
+          valueRange.set(key, String(item.temp[i]));
+        }
 
-      bool success = GSheet.values.append(&response /* returned response */, spreadsheetId /* spreadsheet Id to update */, "Sheet1!A1" /* range to update */, &valueRange /* data to update */);
+        bool success = GSheet.values.append(&response /* returned response */, spreadsheetId /* spreadsheet Id to update */, "Sheet1!A1" /* range to update */, &valueRange /* data to update */);
 
-      response.toString(Serial, true);
-      Serial.println();
+        response.toString(Serial, true);
+        Serial.println();
 
-      if (success) {
-        valueRange.clear();
-        response.clear();
-        vTaskDelay(pdMS_TO_TICKS(1000));
-
+        if (success) {
+          valueRange.clear();
+          response.clear();
+        }
       }
     }
   }
@@ -91,8 +122,6 @@ void setup() {
   Serial.println(WiFi.localIP());
   Serial.println();
 
-  // In case SD/SD_MMC storage file access, mount the SD/SD_MMC card.
-  // SD_Card_Mounting(); // See src/GS_SDHelper.h
 
   GSheet.setCert(rootCACert);  // or GSheet.setCertFile("path/to/certificate/file.pem", esp_google_sheet_file_storage_type_flash /* or esp_google_sheet_file_storage_type_sd */);
 
@@ -105,24 +134,26 @@ void setup() {
   // Begin the access token generation for Google API authentication
   GSheet.begin(CLIENT_EMAIL, PROJECT_ID, PRIVATE_KEY);
 
-  //sincronizando relogios
-  configTime(0, 3600, ntpServer);
-  bool time_ok = getLocalTime(&timeinfo);
-  
-  if(!time_ok){
-    Serial.println("ATT: FALHA EM OBTER TEMPO LOCAL");
-  }
 
   Serial.print("Spreedsheet id: ");
   Serial.println(spreadsheetId);
 
+  // configurando timezone
+  // a priori, as bibliotecas utilizadas aqui ja sincronizam o relogio com sntp
+
+  setenv("TZ", "<-03>3", 1);  // horario de fortaleza
+  Serial.println("Time zone: America/Fortaleza");
+
   Serial.println("Lauching tasks ...");
 
+  dados = xQueueCreate(10, sizeof(data));
+
   xTaskCreate(send_data, "send_data", 40096, NULL, 1, NULL);
+  xTaskCreate(read_temp, "read_temp", 4096, NULL, 2, NULL);
 }
 
 void loop() {
-    
+
   // Call ready() repeatedly in loop for authentication checking and processing
   vTaskDelay(100);
 }
